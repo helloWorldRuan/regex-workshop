7ad6e4a1b8ae0af137cc4672372ca195d3c1dc4d
helloWorldRuan <ruan.rodrigues@sptech.school>
2023-05-01 12:12:35 -0300

    create mask and validation regex and fix code

---
+++ js/validacoes.js
@@ -0,0 +1,46 @@
+function validarEmail() {
+    const email = in_email.value.trim()
+    const validEmail = /^(\w{2,})+([._]?\w+)*@(\w{3,})+([.-])[\w]{2,}$/.test(email)
+
+    if (validEmail) {
+        alert("✅ Email válido")
+    } else {
+        alert("⚠️ Email inválido")
+    }
+}
+
+function validarSenha() {
+    const senha = in_senha.value.trim()
+
+    const hasUppercase = /[A-Z]/g.test(senha)
+    const hasLowercase = /[a-z]/g.test(senha)
+    const hasNumbers = /[0-9]/g.test(senha)
+    const hasSpecialChar = /\D/g.test(senha)
+
+    const validSenha = hasUppercase && hasLowercase && hasNumbers && hasSpecialChar
+
+    if (validSenha) {
+        alert("✅ Senha válida")
+    } else {
+        alert("⚠️ Senha inválida")
+    }
+}
+
+let status = false
+function toggleVisibilidadeSenha() {
+    const input = document.getElementById("in_senha")
+    document.querySelector(".ph-eye").classList.toggle("ph-eye-slash")
+
+    if (status) {
+        input.setAttribute('type', 'password')
+        status = false
+    } else {
+        input.setAttribute('type', 'text')
+        status = true
+    }
+}
+
+function cadastrar() {
+    validarEmail()
+    validarSenha()
+}
